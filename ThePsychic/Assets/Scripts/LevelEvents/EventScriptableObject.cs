b92d32d82cc9ec748dd720abdcf83ccc81d20329
JelloDog <lu.emmanuel@laposte.net>
2020-09-18 15:32:20 +0200

    Event system

---
+++ ThePsychic/Assets/Scripts/LevelEvents/EventScriptableObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Event")]
+public class EventScriptableObject : ScriptableObject
+{
+    public float speed;
+    public Vector3 minPos, maxPos;
+    public Sprite eventSprite;
+    public AudioClip eventSound;
+}
---
+++ ThePsychic/Assets/Scripts/LevelEvents/PredictedEvent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PredictedEvent : MonoBehaviour
+{
+    [SerializeField] EventScriptableObject _event;
+    [SerializeField] TransformData _playerPos;
+
+    private SpriteRenderer _sprite;
+    private Rigidbody2D _rigidbody;
+    private bool isActive;
+
+    private void Awake()
+    {
+        transform.position = _playerPos.value.position + _event.minPos;
+        _sprite.sprite = _event.eventSprite;
+    }
+
+    public void StartEvent()
+    {
+        isActive = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isActive)
+        {
+            Movement();
+        }
+    }
+
+    private void Movement()
+    {
+        _rigidbody.MovePosition((_playerPos.value.position + _event.maxPos)* _event.speed* Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //collision.gameObject.GetComponent<Death>()
+        }
+    }
+
+
+}
