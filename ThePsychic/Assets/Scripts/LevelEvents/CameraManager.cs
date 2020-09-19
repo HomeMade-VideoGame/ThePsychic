b390a523562f118a9d252acf2303b0ad0b45ef12
Bikarbonate <vsenneville@yahoo.fr>
2020-09-19 11:32:20 +0200

    Camera Zoom Out 1.5

--- ThePsychic/Assets/Scripts/LevelEvents/CameraManager.cs
+++ ThePsychic/Assets/Scripts/LevelEvents/CameraManager.cs
@@ -1,13 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Runtime.CompilerServices;
 using Cinemachine;
 
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera _cam1;
     [SerializeField] private CinemachineVirtualCamera _cam2;
-    [SerializeField] private CinemachineVirtualCamera _cam3;
     [SerializeField] private Camera _mainCamera;
     [SerializeField] private float _camSwapTime;
 
@@ -15,7 +13,6 @@ public class CameraManager : MonoBehaviour
     {
         _cam1.gameObject.SetActive(false);
         _cam2.gameObject.SetActive(true);
-        _cam3.gameObject.SetActive(false);
     }
 
     private void Update()
@@ -23,24 +20,15 @@ public class CameraManager : MonoBehaviour
         StartCoroutine(CamSwapTime());
     }
 
-    private void Camera2To3()
-    {
-        _cam3.gameObject.SetActive(true);
-        _cam2.gameObject.SetActive(false);
-    }
-
-    private void Camera3To1()
+    private void CameraSwap()
     {
         _cam1.gameObject.SetActive(true);
-        _cam3.gameObject.SetActive(false);
+        _cam2.gameObject.SetActive(false);
     }
 
     IEnumerator CamSwapTime()
     {
         yield return new WaitForSeconds(_camSwapTime);
-        Camera2To3(); 
-
-        yield return new WaitForSeconds(_camSwapTime);
-        Camera3To1();
+        CameraSwap();
     }
 }
