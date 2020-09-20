ab5412f921efc4cc8567889dabeef7f8871d302b
Bikarbonate <vsenneville@yahoo.fr>
2020-09-21 00:37:25 +0200

    bn

--- ThePsychic/Assets/Scripts/LevelEvents/CameraManager.cs
+++ ThePsychic/Assets/Scripts/LevelEvents/CameraManager.cs
@@ -6,9 +6,6 @@ public class CameraManager : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera _cam1;
     [SerializeField] private CinemachineVirtualCamera _cam2;
-    [SerializeField] private CinemachineVirtualCamera _cam2bis;
-    [SerializeField] private CinemachineVirtualCamera _cam3;
-    [SerializeField] private CinemachineVirtualCamera _cam4;
     [SerializeField] private Camera _mainCamera;
     [SerializeField] private float _camSwapTime;
     [SerializeField] private RandomEvent _randomEvent;
@@ -17,9 +14,6 @@ public class CameraManager : MonoBehaviour
     {
         _cam1.gameObject.SetActive(false);
         _cam2.gameObject.SetActive(true);
-        _cam2bis.gameObject.SetActive(false);
-        _cam3.gameObject.SetActive(false);
-        _cam4.gameObject.SetActive(false);
     }
 
     private void Update()
@@ -28,20 +22,20 @@ public class CameraManager : MonoBehaviour
         //StartTravelling();
     }
 
-    public void StartTravelling()
-    {
-        if (Mathf.Approximately(_mainCamera.transform.position.x, _cam3.transform.position.x))
-        {
-            _cam3.gameObject.SetActive(true);
-            _cam4.gameObject.SetActive(false);
-        }
+    //public void StartTravelling()
+    //{
+    //    if (Mathf.Approximately(_mainCamera.transform.position.x, _cam3.transform.position.x))
+    //    {
+    //        _cam3.gameObject.SetActive(true);
+    //        _cam4.gameObject.SetActive(false);
+    //    }
 
-        if (Mathf.Approximately(_mainCamera.transform.position.x, _cam4.transform.position.x))
-        {
-            _cam3.gameObject.SetActive(false);
-            _cam4.gameObject.SetActive(true);
-        }
-    }
+    //    if (Mathf.Approximately(_mainCamera.transform.position.x, _cam4.transform.position.x))
+    //    {
+    //        _cam3.gameObject.SetActive(false);
+    //        _cam4.gameObject.SetActive(true);
+    //    }
+    //}
 
     private void CameraSwap()
     {
