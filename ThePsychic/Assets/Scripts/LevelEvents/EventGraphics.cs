cab45cc76985e2ff49031d852b20dea840bcb22f
JelloDog <lu.emmanuel@laposte.net>
2020-09-20 23:37:13 +0200

    SOUND UPDATE

--- ThePsychic/Assets/Scripts/LevelEvents/EventGraphics.cs
+++ ThePsychic/Assets/Scripts/LevelEvents/EventGraphics.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class EventGraphics : MonoBehaviour
 {
     [SerializeField] AudioClip _eventSound, _eventDefuse, _eventTrigger;
+    [SerializeField] float _soundRadius;
     private Animator _anim;
     private AudioSource _audio;
+    private Collider2D hit;
 
     public void Start()
     {
         _anim = GetComponent<Animator>();
         _audio = GetComponent<AudioSource>();
-        _audio.clip = _eventSound; _audio.Play();
+    }
+
+    private void Update()
+    {
+        hit = Physics2D.OverlapCircle(transform.position, _soundRadius, LayerMask.NameToLayer("Player"));
+        if (hit.CompareTag("Player"))
+        {
+            Debug.Log("Player in sound zone");
+            TriggerHintSound();
+        }
     }
 
 
+    private void TriggerHintSound()
+    {
+        _audio.clip = _eventSound; _audio.Play();
+    }
 
     public void AnimateDeath()
     {
